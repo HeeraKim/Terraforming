4610ec1eb8566365e9b1d4e2eb73f919a60cf9a1
Rin <pomeryt@naver.com>
2017-12-25 22:49:56 +0900

    add title

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,6 @@ import application.page.initial.Initial;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
--- src/application/page/initial/BaseInitial.java
+++ src/application/page/initial/BaseInitial.java
@@ -1,8 +1,10 @@
 package application.page.initial;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
@@ -14,30 +16,56 @@ import javafx.scene.layout.StackPane;
 public class BaseInitial extends Initial {
 	
 	@Override
-	public Scene scene() {
+	public Scene scene() {	
 		// Width for buttons
-		final double buttonWidth = 100;
+		final double buttonWidth = 200;
+		
+		// Height for buttons
+		final double buttonHeight = 75;
+		
+		// Vertical gap between buttons
+		final double vGapButtons = 30;
+		
+		// Button text size
+		final double buttonTextSize = 32;
+		
+		// Label text size
+		final double titleTextSize = 100;
+		
+		// Label to display main title
+		final Label lTitle = new Label("Terraforming");
+		lTitle.setStyle("-fx-font-size: "+titleTextSize);
 		
 		// Button for new game
 		final Button bNew = new Button("New");
 		bNew.setPrefWidth(buttonWidth);
+		bNew.setPrefHeight(buttonHeight);
+		bNew.setStyle("-fx-font-size: "+buttonTextSize);
 		
 		// Button for continue
 		final Button bContinue = new Button("Continue");
 		bContinue.setPrefWidth(buttonWidth);
+		bContinue.setPrefHeight(buttonHeight);
+		bContinue.setStyle("-fx-font-size: "+buttonTextSize);
 		
 		// Button for setting
 		final Button bSetting = new Button("Setting");
 		bSetting.setPrefWidth(buttonWidth);
+		bSetting.setPrefHeight(buttonHeight);
+		bSetting.setStyle("-fx-font-size: "+buttonTextSize);
 		
 		// GridPane to store buttons
 		final GridPane gridButtons = new GridPane();
 		gridButtons.addColumn(0, bNew, bContinue, bSetting);
-		gridButtons.setAlignment(Pos.CENTER);
+		gridButtons.setAlignment(Pos.BOTTOM_CENTER);
+		gridButtons.setVgap(vGapButtons);
+		gridButtons.setPadding(new Insets(0, 0, 30, 0));
 		
 		// Root container
 		final StackPane pane = new StackPane();
 		pane.getChildren().add(gridButtons);
+		pane.getChildren().add(lTitle);
+		StackPane.setAlignment(lTitle, Pos.TOP_CENTER);
 		
 		// Scene
 		final Scene scene = new Scene(pane);
